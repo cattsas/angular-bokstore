5fe1efadcac110717ee02e0a905b26af81c68de5
cattsas <cattsas1980@gmail.com>
2020-11-17 02:21:31 -0300

    Se implementan directivas para generar interfaz gráfica

---
+++ src/app/book-list/Book.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    nombre:string;
+    autor :string;
+    genero:string;
+    editorial:string;
+    precio:number;
+    imagen:string;
+    cantidad:number;
+    best_seller:boolean;
+}
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Book } from './Book';
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
 export class BookListComponent implements OnInit {
-  book = {
+  books: Book[] =[ 
+      {
         "nombre":"El Psicoanalista",
         "autor" :"John Katzenbach",
         "genero":"Thriller",
         "editorial":"Kapelusz",
-        "precio":"1500",
+        "precio":1500,
         "imagen":"assets/images/psicoanalista.jpg",
-        "cantidad":50
-  }
+        "cantidad":50,
+        "best_seller":true
+      },
+    {
+      "nombre":"Cujo",
+      "autor" :"Stephen King",
+      "genero":"Terror",
+      "editorial":"LB",
+      "precio":1100,
+      "imagen":"assets/images/cujo.jpg",
+      "cantidad":0,
+      "best_seller":false
+  }]
   constructor() { }
 
   ngOnInit(): void {
