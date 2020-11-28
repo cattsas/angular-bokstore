d107eb6df209fb54b720f694d3f0ad50fe788d75
cattsas <cattsas1980@gmail.com>
2020-11-28 17:25:17 -0300

    Se cargan mas elementos a la lista

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -18,6 +18,46 @@ export class BookListComponent implements OnInit {
         "best_seller":true,
         "cantidad":0
       },
+      {
+        "nombre":"La guerra invisible",
+        "autor" :"Marcelo Larraquy",
+        "genero":"Investigación",
+        "precio":1200,
+        "imagen":"assets/images/guerra invisible.jpg",
+        "stock":12,
+        "best_seller":false,
+        "cantidad":0
+      },
+      {
+        "nombre":"Los guardianes",
+        "autor" :"John Grisham",
+        "genero":"Suspenso",
+        "precio":1300,
+        "imagen":"assets/images/los guardianes.jpg",
+        "stock":12,
+        "best_seller":true,
+        "cantidad":0
+      },
+      {
+        "nombre":"Lo mucho que te amé",
+        "autor" :"Eduardo Sacheri",
+        "genero":"Novela",
+        "precio":1000,
+        "imagen":"assets/images/sacheri.jpg",
+        "stock":25,
+        "best_seller":false,
+        "cantidad":0
+      },
+      {
+        "nombre":"Línea de fuego",
+        "autor" :"Arturo Perez-Reverte",
+        "genero":"Novela histórica",
+        "precio":1700,
+        "imagen":"assets/images/reverte.jpg",
+        "stock":17,
+        "best_seller":true,
+        "cantidad":0
+      },
     {
       "nombre":"Cujo",
       "autor" :"Stephen King",
@@ -25,7 +65,7 @@ export class BookListComponent implements OnInit {
       "precio":1100,
       "imagen":"assets/images/cujo.jpg",
       "stock":15,
-      "best_seller":false,
+      "best_seller":true,
       "cantidad":0
   }];
  
